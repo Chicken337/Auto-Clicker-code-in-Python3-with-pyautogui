ae9d98dc3b44db599f2741b3822237f76514d48d
Titus <120826876+Chicken337@users.noreply.github.com>
2023-03-20 10:00:48 -0500

    Add files via upload

---
+++ CPS cheats.py
@@ -0,0 +1,12 @@
+import pyautogui
+pyautogui.PAUSE = 0.001
+import time
+print ("Get Ready!")
+time.sleep (.5)
+print ("go to the clicker project and click the green flag on the project")
+time.sleep (1)
+print ("starting code...")
+time.sleep (3)
+for i in range(400):
+    pyautogui.mouseDown(button='left')
+    pyautogui.mouseUp(button='left')
